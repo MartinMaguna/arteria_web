3e0f182382c23ba161010d21110f126ab414c58c
Martín Maguna <martinmaguna33@gmail.com>
2024-12-29 22:35:15 -0300

    de app a index

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <header>
-        <nav>
-          <ul>
-            <li><a href="#ensayo">Ensayo</a></li>
-            <li><a href="#poesia">Poesía</a></li>
-            <li><a href="#posdata">Posdata</a></li>
-          </ul>
-        </nav>
-      </header>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <>     
       <h1>Arteria</h1>
       <div className="card">  
         <button onClick={() => setCount((count) => count + 1)}>
@@ -37,9 +18,6 @@ function App() {
       <p className="read-the-docs">
         Probando cambios
       </p>
-      <footer>
-        <h2>Arteria</h2>
-      </footer>
     </>
   )
 }
