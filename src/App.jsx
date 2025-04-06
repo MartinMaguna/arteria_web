925ba0d7461ac88666db3cf12c756f6b0ebdaa1d
Martina Maguna <martinmaguna33@gmail.com>
2025-04-06 13:19:17 -0300

    ajustes abril 5

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Ensayos from "./Componentes/Ensayos/Ensayos";
 import EnsayoIndividual from "./Componentes/EnsayoIndividual/EnsayoIndividual";
 import Poesia from "./Componentes/Poesia/Poesia";
 import PoemaDigital from "./Componentes/PoemaDigital/PoemaDigital";
-import Posdata from "./Componentes/Posdata/Posdata";
 import DataArtworkViewer from "./Componentes/DataArtworkViewer/DataArtworkViewer";
 import About from "./Componentes/About/About";
 import { Footer } from "./Componentes/Footer/Footer";
